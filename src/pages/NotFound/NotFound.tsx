865ffc6cc37c14199baf599ec8fe12bf6fd85292
hwnsng <amc214677@gmail.com>
2025-11-05 00:06:13 +0900

    fix: goBack 함수 삭제

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -3,12 +3,11 @@ import { useNavigate } from 'react-router';
 
 export default function NotFound(){
   const navigate = useNavigate();
-  const goBack = () => navigate(-1);
 
   return(
     <div className="min-w-full h-[99vh]">
       <div className="flex min-w-full pl-20 h-[10%] items-end">
-        <div className="flex items-center cursor-pointer w-36 h-9" onClick={goBack}>
+        <div className="flex items-center cursor-pointer w-36 h-9" onClick={() => navigate(-1)}>
           <IoIosArrowBack size={30} />
           <h1 className="flex text-2xl font-bold text-black">
             뒤로가기
