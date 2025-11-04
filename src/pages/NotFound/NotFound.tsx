f646c9d480efd38657b02fcf15f1fb8f9d91f02e
hwnsng <amc214677@gmail.com>
2025-11-04 23:59:35 +0900

    fix: NotFound 페이지 디자인 수정

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,25 @@
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from 'react-router';
+
 export default function NotFound(){
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
   return(
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-black-500">
-        Not Found
-      </h1>
+    <div className="min-w-full h-[99vh]">
+      <div className="flex min-w-full pl-20 h-[10%] items-end">
+        <div className="flex items-center cursor-pointer w-36 h-9" onClick={goBack}>
+          <IoIosArrowBack size={30} />
+          <h1 className="flex text-2xl font-bold text-black">
+            뒤로가기
+          </h1>
+        </div>
+      </div>
+      <div className="flex items-center justify-center min-w-full h-[90%]">
+        <h1 className="text-5xl font-bold text-black">
+          Not Found
+        </h1>
+      </div>
     </div>
   );
 }
