bebd2f121470aedc37ac8aec3939c73359f25a64
hwnsng <amc214677@gmail.com>
2025-11-07 01:21:56 +0900

    feat: 홈화면 배너에 들어갈 메뉴 컴포넌트 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,7 @@
 import Logo from './logo.svg';
 import Person from './person.svg'
+import House1 from './one-room-house.png'
+import House2 from './apart-house.png'
+import House3 from './city-map.png';
 
-export { Logo, Person }
+export { Logo, Person, House1, House2, House3 }
---
+++ src/pages/home/components/BannerItem.tsx
@@ -0,0 +1,42 @@
+import { House1, House2, House3 } from '@/assets/index';
+
+interface ItemProps{
+  title: string,
+  subTitle: string,
+  data: string,
+  shape: number,
+  imgNumber: number,
+  onclick: () => void,
+}
+
+export default function BannerItem(
+  { title, subTitle, data, shape, imgNumber, onclick }: ItemProps
+){
+
+  return(
+    <>
+      {shape === 1 ? 
+        <div onClick={onclick} className="flex w-[266px] h-40 pt-7 pl-[22px] pr-4 justify-between cursor-pointer">
+          <div>
+            <h1 className="text-[16px] text-black font-bold pb-[6px]">{title}</h1>
+            <p className="text-xs font-medium text-[#757575] pb-6px]">{subTitle}</p>
+            <p className="text-[10px] font-medium text-[#757575]">{data}</p>
+          </div>
+          <div className="flex items-center h-full">
+            <img className="w-[62px] h-14" src={imgNumber === 1 ? House1 : House2} alt="집 아이콘" />
+          </div>
+        </div>
+        :
+        <div onClick={onclick} className="flex w-[552]px] h-40 pt-8 pl-[18px] pr-[30px] justify-between cursor-pointer">
+          <div>
+            <h1 className="text-[18px] text-black font-bold pb-[22px]">{title}</h1>
+            <p className="text-[14px] font-medium text-[#757575]">{subTitle}</p>
+          </div>
+          <div className="flex items-center h-full">
+            <img className="w-[154px] h-22" src={House3} alt="도시 맵 아이콘" />
+          </div>
+        </div>
+        }
+    </>
+  )
+}
