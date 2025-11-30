7ee1c254fdf3c89a95ccda62a97ba2d07bf51268
hwnsng <amc214677@gmail.com>
2025-12-01 08:58:08 +0900

    fix: CheckList 컴포넌트 파일 구조 변경

--- src/pages/Checklist/components/index.ts
+++ src/pages/Checklist/components/index.ts
@@ -1,6 +1,6 @@
-export { default as CheckListMenu } from './CheckListMenu';
+export { default as CheckListMenu } from './Main/CheckListMenu';
 export { default as NoData } from './NoData';
-export { default as CheckListAddMenu } from './CheckListAddMenu';
-export { default as CheckListConfirmItem } from './CheckListConfirmItem';
-export { default as AiReportButton } from './AiReportButton';
-export { default as Input } from './CheckListAddInput';
+export { default as CheckListAddMenu } from './Select/CheckListAddMenu';
+export { default as CheckListConfirmItem } from './List/CheckListConfirmItem';
+export { default as AiReportButton } from './AiReport/AiReportButton';
+export { default as Input } from './Create/CheckListAddInput';
